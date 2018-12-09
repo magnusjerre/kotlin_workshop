c542fbd60498cf1004beeeee790368a61a445101
Magnus Jerre <magnus.jerre@gmail.com>
2018-12-09 10:50:09 +0100

    Legger til overrides på Person-klassen

--- src/main/java/jerre/kotlin/workshop/models/Person.java
+++ src/main/java/jerre/kotlin/workshop/models/Person.java
@@ -2,6 +2,7 @@ package jerre.kotlin.workshop.models;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Objects;
 import java.util.Optional;
 
 public class Person implements Id {
@@ -57,4 +58,30 @@ public class Person implements Id {
         return Optional.ofNullable(address);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) &&
+                Objects.equals(birthDate, person.birthDate) &&
+                Objects.equals(name, person.name) &&
+                Objects.equals(address, person.address);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, birthDate, name, address);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", birthDate=" + birthDate +
+                ", name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
 }
